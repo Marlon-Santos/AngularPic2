1f766554163437df48dc3cf18740d1292f579c07
Marlon-Santos <marlonergondossantos@gmail.com>
2020-06-11 17:43:10 -0300

    code refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Photos } from './photos/photo/model/Photos.interface';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'AngularPic2';
+export class AppComponent implements OnInit {
+  photos: Photos[];
+
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit(): void {
+    this.photoService
+      .getPhotos('flavio')
+      .subscribe((photos) => (this.photos = photos));
+  }
 }
