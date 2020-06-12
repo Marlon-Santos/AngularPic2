29b6d929b4e56a38c9378ef5c64c578a972df27b
Marlon-Santos <marlonergondossantos@gmail.com>
2020-06-12 00:54:04 -0300

    create photos service and interface

--- src/app/photos/model/Photos.interface.ts
+++ src/app/photos/model/Photos.interface.ts
@@ -2,6 +2,7 @@ export interface Photos {
   allowComments: boolean;
   comments: number;
   description: string;
+  url: string;
   id: number;
   likes: number;
   postDate: Date;
--- src/app/photos/service/photo.service.ts
+++ src/app/photos/service/photo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Photos } from './model/Photos.interface';
+import { Photos } from '../model/Photos.interface';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
@@ -12,6 +12,7 @@ const URL_PHOTOS = (name: string) => `${environment.URL_PHOTOS}/${name}/photos`;
 })
 export class PhotoService {
   constructor(private http: HttpClient) {}
+
   getPhotos(user: string): Observable<Photos[]> {
     return this.http.get<Photos[]>(URL_PHOTOS(user)).pipe(take(1));
   }
