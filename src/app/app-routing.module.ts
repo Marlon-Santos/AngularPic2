8c976fd2b3366b6e02488e251f29c43465da5693
Marlon-Santos <marlonergondossantos@gmail.com>
2020-06-18 12:32:00 -0300

    add new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/resolver/photo-list.resolver';
-import { SingInComponent } from './sign-in/sign-in.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { SingInComponent } from './sign/sign-in/sign-in.component';
+import { SignUpComponent } from './sign/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SingInComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent,
   },
   {
     path: 'user/:userName/photos',
